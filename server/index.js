8dffc448764c925f770f425be210629c048d6356
Christian Zigah <zigahchristian@yahoo.com>
2023-12-12 21:12:13 +0000

    Added a new express server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+const port = process.env.PORT || 7242;
+
+app.listen(
+  port,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
+
+module.exports = app;
