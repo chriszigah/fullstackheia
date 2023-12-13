5d14629911c7c44e4dff58c625473d64d8c78e55
Christian Zigah <zigahchristian@yahoo.com>
2023-12-13 09:34:41 +0000

    Add vercel backend

--- app/src/helpers/http-common.js
+++ app/src/helpers/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:7240",
+  baseURL: "https://heia-sms-api.vercel.app",
   withCredentials: true,
   headers: {
     "content-type": "application/json",
