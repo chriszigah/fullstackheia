8d641187f31284f35e00b25c1b2bd7b1f6b7c4ac
Christian Zigah <zigahchristian@yahoo.com>
2023-12-14 21:35:43 +0000

    Material Css Done

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -5,9 +5,6 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store.jsx";
-import(
-  `https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0`
-);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
